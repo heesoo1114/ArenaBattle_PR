c2de4d02650928bc5a148709ceff17bf4b7c6565
Yoon HeeSoo <improveme7@gmail.com>
2024-03-11 16:15:59 +0900

    create project

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "Player/ABPlayerController.h"
+
+AABGameMode::AABGameMode()
+{
+	// Default Pawn Class
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(
+		TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+
+	// Player Controller Class
+	PlayerControllerClass = AABPlayerController::StaticClass();
+}
