16d6837e59c2f072f000f8ea873da43564f28a57
Yoon HeeSoo <improveme7@gmail.com>
2024-03-18 11:01:10 +0900

    player basic input

--- Source/ArenaBattle/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattle/Character/ABCharacterPlayer.cpp
@@ -4,6 +4,9 @@
 #include "Character/ABCharacterPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 AABCharacterPlayer::AABCharacterPlayer()
 {
@@ -16,4 +19,81 @@ AABCharacterPlayer::AABCharacterPlayer()
     FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
     FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
     FollowCamera->bUsePawnControlRotation = false;
+
+    // Input
+    static ConstructorHelpers::FObjectFinder<UInputMappingContext> InputMappingContextRef(TEXT(
+        "/Game/ArenaBattle/Input/IMC_Default.IMC_Default"));
+    if (InputMappingContextRef.Object)
+    {
+        DefaultMappingContext = InputMappingContextRef.Object;
+    }
+    
+    static ConstructorHelpers::FObjectFinder<UInputAction> JumpActionRef(TEXT(
+        "/Game/ArenaBattle/Input/Actions/IA_Jump.IA_Jump"));
+    if (JumpActionRef.Object)
+    {
+        JumpAction = JumpActionRef.Object;
+    }
+
+    static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT(
+        "/Game/ArenaBattle/Input/Actions/IA_Move.IA_Move"));
+    if (MoveActionRef.Object)
+    {
+        MoveAction = MoveActionRef.Object;
+    }
+
+    static ConstructorHelpers::FObjectFinder<UInputAction> LookActionRef(TEXT(
+        "/Game/ArenaBattle/Input/Actions/IA_Look.IA_Look"));
+    if (LookActionRef.Object)
+    {
+        LookAction = LookActionRef.Object;
+    }
+}
+
+void AABCharacterPlayer::BeginPlay()
+{
+    Super::BeginPlay();
+
+    APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
+
+    // PlayerController의 Subsystem을 가져와서 InputMappingContext 설정
+    UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+    if (Subsystem)
+    {
+        Subsystem->AddMappingContext(DefaultMappingContext, 0);
+    }
+}
+
+void AABCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+    EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+    EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Move);
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Look);
+}
+
+void AABCharacterPlayer::Move(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+
+    const FRotator Rotation = Controller->GetControlRotation();
+    const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+    const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+    const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+    AddMovementInput(ForwardDirection, MovementVector.X);
+    AddMovementInput(RightDirection, MovementVector.Y);
+}
+
+void AABCharacterPlayer::Look(const FInputActionValue& Value)
+{
+    FVector2D LookAxisVector = Value.Get<FVector2d>();
+    AddControllerYawInput(LookAxisVector.X);
+    AddControllerPitchInput(LookAxisVector.Y);
 }
